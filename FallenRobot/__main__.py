b9d7482f1e112b5edd7b3c623175347a84487a1e
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-04-12 03:31:54 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -197,7 +197,11 @@ def start(update: Update, context: CallbackContext):
             elif args[0][1:].isdigit() and "rᴜʟᴇs" in IMPORTED:
                 IMPORTED["rᴜʟᴇs"].send_rules(update, args[0], from_pm=True)
 
-
+        else:
+            first_name = update.effective_user.first_name
+            update.effective_message.reply_sticker(
+                "CAACAgEAAxkBAAECq7RkNdhOP2Nrfm_RBCpxzl9b5EacywACHAMAAhZIsUVV189Fb6pbsi8E"
+            )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
                 reply_markup=InlineKeyboardMarkup(buttons),
