8ff057771a51402e96a1f4e1366fb8b5ea6c4e53
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-04-12 16:19:54 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -713,13 +713,13 @@ def main():
                 chat_id=f"@{SUPPORT_CHAT}",
                 photo=START_IMG,
                 caption=f"""
-**✅ sᴇxʏ ǫᴜᴇᴇɴ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ ✅**\n
-     ┏━━━━━━━━━━━━━━━┓
-     ┣➤ **ᴄʀᴇᴀᴛᴏʀ** :: **[sᴀɴɢʀᴀᴍ](https://t.me/Backup_Bosss)**
-     ┣➤ **ᴏᴡɴᴇʀ** :: **[sᴀɴɢʀᴀᴍ](https://t.me/Kalakar_Sangram)**
-     ┣➤ **sᴜᴘᴘᴏʀᴛ** :: **[sᴜᴘᴘᴏʀᴛ](https://t.me/Red_Wine_Op)**
-     ┣➤ **ᴜᴘᴅᴀᴛᴇs** :: **[ᴜᴘᴅᴀᴛᴇs](https://t.me/WCFnetwork)**
-     ┗━━━━━━━━━━━━━━━┛
+**✅sᴇxʏ ǫᴜᴇᴇɴ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ✅**
+┏━━━━━━━━━━━━━━━┓
+┣➤ **ᴄʀᴇᴀᴛᴏʀ** :: **[sᴀɴɢʀᴀᴍ](https://t.me/Backup_Bosss)**
+┣➤ **ᴏᴡɴᴇʀ** :: **[sᴀɴɢʀᴀᴍ](https://t.me/Kalakar_Sangram)**
+┣➤ **sᴜᴘᴘᴏʀᴛ** :: **[sᴜᴘᴘᴏʀᴛ](https://t.me/Red_Wine_Op)**
+┣➤ **ᴜᴘᴅᴀᴛᴇs** :: **[ᴜᴘᴅᴀᴛᴇs](https://t.me/WCFnetwork)**
+┗━━━━━━━━━━━━━━━┛
 """,
                 parse_mode=ParseMode.MARKDOWN,
             )
