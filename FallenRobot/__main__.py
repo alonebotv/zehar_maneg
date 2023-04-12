2ce8879b0c587200f992609a3a9f1478b463d293
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-04-12 16:55:49 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -738,7 +738,7 @@ def main():
     settings_handler = CommandHandler("settings", get_settings)
     settings_callback_handler = CallbackQueryHandler(settings_button, pattern=r"stngs_")
 
-    Music_callback_handler = CallbackQueryHandler(
+    about_callback_handler = CallbackQueryHandler(
         Music_about_callback, pattern=r"fallen_"
     )
     Music_callback_handler = CallbackQueryHandler(
@@ -750,7 +750,7 @@ def main():
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(help_handler)
-    dispatcher.add_handler(Music_callback_handler)
+    dispatcher.add_handler(about_callback_handler)
     dispatcher.add_handler(Music_callback_handler)
     dispatcher.add_handler(settings_handler)
     dispatcher.add_handler(help_callback_handler)
