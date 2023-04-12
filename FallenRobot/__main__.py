fa17e31d2c56628c16bb90904a32089e7a49f099
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-04-12 11:58:38 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -199,8 +199,7 @@ def start(update: Update, context: CallbackContext):
 
         else:
             first_name = update.effective_user.first_name
-            update.effective_message.reply_sticker(
-                "CAACAgEAAxkBAAECq7RkNdhOP2Nrfm_RBCpxzl9b5EacywACHAMAAhZIsUVV189Fb6pbsi8E"
+
             )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
