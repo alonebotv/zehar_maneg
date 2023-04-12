9882fb72fd4e5bf6d3d196cf2cb1b3321e4eeef3
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-04-12 19:59:14 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -201,7 +201,7 @@ def start(update: Update, context: CallbackContext):
 
 
             update.effective_message.reply_text(
-                PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
+                PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME, START_IMG),
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
