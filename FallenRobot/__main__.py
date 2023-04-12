a19175650d7a918fee9a5a01753fc72b3542be9d
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-04-12 15:14:17 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -713,7 +713,7 @@ def main():
                 chat_id=f"@{SUPPORT_CHAT}",
                 photo=START_IMG,
                 caption=f"""
-ㅤ🥀 {BOT_NAME} ɪs ᴀʟɪᴠᴇ ʙᴀʙʏ...
+**✅ sᴇxʏ ǫᴜᴇᴇɴ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ ✅**
 
 ┏•❅────✧❅✦❅✧────❅•┓
 ㅤ★ **ᴘʏᴛʜᴏɴ :** `{y()}`
